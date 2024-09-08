c9136447a5def92e1b02eea83c84f78c6f0f18dd
Aricatri <aricatri@gmail.com>
2024-09-07 22:34:33 -0500

    feat: solution revising-strings.js

---
+++ revising-strings.js
@@ -0,0 +1,9 @@
+// Define a variable named pizza
+const pizza = 'pizza is alright';
+
+// Use .replace() to change 'alright' to 'wonderful'
+const newPizza = pizza.replace('alright', 'wonderful');
+
+// Print the result to the terminal
+console.log(newPizza);
+
