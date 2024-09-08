2eec50dfa90619d90e8a476004f1c53820215f6d
Aricatri <aricatri@gmail.com>
2024-09-07 23:43:07 -0500

    feat:solution for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+let total = 0;  // Cambiado a let, porque se va a modificar
+const limit = 10;
+
+for (let i = 0; i < limit; i++) {
+  total += i;  // Acumula el valor de i en total
+}
+
+console.log(total);  // Imprime el total
